3dc86fdf2ad3ead0f50aafdb57ba76d4cf234162
yolkaSD <yolka.sd@gmail.com>
2023-09-03 11:57:28 +0700

    Basic Application Structure

---
+++ src/main/java/com/example/people_tracker/PeopleTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.people_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeopleTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeopleTrackerApplication.class, args);
+	}
+
+}
