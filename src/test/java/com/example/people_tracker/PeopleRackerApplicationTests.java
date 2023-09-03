a994d5f51c288460f8a58fee854f09af2caac9c7
yolkaSD <yolka.sd@gmail.com>
2023-09-03 12:55:29 +0700

    Created a connection to the database

---
+++ src/test/java/com/example/people_tracker/PeopleRackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.people_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeopleRackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
